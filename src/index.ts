5e5c4fc38307b196d4d81f7ceee607193a2755cb
Paolo Manaois <paolo.manaois@codeworks.fr>
2023-10-10 14:45:41 +0200

    [001]: added linter, formatter and git hooks

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import Express from 'express';
 const app = Express();
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
+	console.log(`Listening on port ${port}.`);
 });
