eabfb4158e76a11db3aa525fbf5b02f34b5bde01
Paolo Manaois <paolo.manaois@codeworks.fr>
2023-10-10 17:02:22 +0200

    [003]: hero creation endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import BodyParser from 'body-parser';
 import Express from 'express';
 import { HeroService } from './services/HeroService';
 
 const app = Express();
 const port = process.env.PORT || 3000;
 const heroService = new HeroService();
+
+app.use(BodyParser.json());
+
+app.post('/heroes', async (req, res) => {
+	const { name } = req.body;
+	const hero = await heroService.addHero(name);
+	res.status(201).json(hero);
+});
 
 app.get('/heroes', async (_, res) => {
 	res.json(await heroService.listHeroes());
--- src/services/HeroService.ts
+++ src/services/HeroService.ts
@@ -1,19 +1,38 @@
 import { Hero } from '../types/Hero';
 
+const allHeroes: Hero[] = [
+	{ id: 12, name: 'Dr. Nice' },
+	{ id: 13, name: 'Bombasto' },
+	{ id: 14, name: 'Celeritas' },
+	{ id: 15, name: 'Magneta' },
+	{ id: 16, name: 'RubberMan' },
+	{ id: 17, name: 'Dynama' },
+	{ id: 18, name: 'Dr. IQ' },
+	{ id: 19, name: 'Magma' },
+	{ id: 20, name: 'Tornado' },
+];
+
 export class HeroService {
-	private data: Hero[] = [
-		{ id: 12, name: 'Dr. Nice' },
-		{ id: 13, name: 'Bombasto' },
-		{ id: 14, name: 'Celeritas' },
-		{ id: 15, name: 'Magneta' },
-		{ id: 16, name: 'RubberMan' },
-		{ id: 17, name: 'Dynama' },
-		{ id: 18, name: 'Dr. IQ' },
-		{ id: 19, name: 'Magma' },
-		{ id: 20, name: 'Tornado' },
-	];
+	private data = new Map<number, Hero>(
+		allHeroes.map((hero) => [hero.id, hero]),
+	);
+
+	async addHero(name: string): Promise<Hero> {
+		const hero: Hero = {
+			id: this.newId(),
+			name,
+		};
+
+		this.data.set(hero.id, hero);
+
+		return hero;
+	}
 
 	async listHeroes(): Promise<Hero[]> {
-		return this.data;
+		return Array.from(this.data.values());
+	}
+
+	private newId(): number {
+		return this.data.size > 0 ? Math.max(...this.data.keys()) + 1 : 11;
 	}
 }
