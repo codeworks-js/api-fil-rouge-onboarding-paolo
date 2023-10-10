dabbab130d60e0b2bdf1ad4084719b5075726f1e
Paolo Manaois <paolo.manaois@codeworks.fr>
2023-10-10 16:31:53 +0200

    [002]: heroes list endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Express from 'express';
+import { HeroService } from './services/HeroService';
 
 const app = Express();
 const port = process.env.PORT || 3000;
+const heroService = new HeroService();
 
-app.get('/', (_, res) => {
-	res.send('Hello, World!');
+app.get('/heroes', async (_, res) => {
+	res.json(await heroService.listHeroes());
 });
 
 app.listen(port, () => {
